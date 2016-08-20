8154d0a5eb8e6652d5f52c815825755eb3b59fa8
sourav sarkar <souravsrkr254@gmail.com>
2016-08-21 00:41:46 +0530

    assignment completed

--- CurrencyConverter/ViewController.m
+++ CurrencyConverter/ViewController.m
@@ -7,14 +7,17 @@
 //
 
 #import "ViewController.h"
+#import "CurrencyRequest/CRCurrencyRequest.h"
+#import "CurrencyRequest/CRCurrencyResults.h"
 
-@interface ViewController ()
+@interface ViewController () <CRCurrencyRequestDelegate>
+
 @property (weak, nonatomic) IBOutlet UITextField *inputField;
 @property (weak, nonatomic) IBOutlet UIButton *convertButton;
 @property (weak, nonatomic) IBOutlet UILabel *outputLabelUSD;
 @property (weak, nonatomic) IBOutlet UILabel *outputLabelGBP;
 @property (weak, nonatomic) IBOutlet UILabel *outputLabelJPY;
-
+@property (nonatomic) CRCurrencyRequest *req;
 @end
 
 @implementation ViewController
@@ -29,10 +32,37 @@
     // Dispose of any resources that can be recreated.
 }
 - (IBAction)convertButtonTapped:(id)sender {
+    
     self.convertButton.enabled = NO;
-    self.outputLabelUSD.text = @"usd";
-    self.outputLabelGBP.text = @"gbp";
-    self.outputLabelJPY.text = @"jpy";
+    self.req = [[CRCurrencyRequest alloc] init];
+    self.req.delegate = self;
+    [self.req start];
+    
+    
+//    self.outputLabelUSD.text = @"usd";
+//    self.outputLabelGBP.text = @"gbp";
+//    self.outputLabelJPY.text = @"jpy";
+}
+
+- (void) currencyRequest:(CRCurrencyRequest *)req retrievedCurrencies:(CRCurrencyResults *)currencies {
+    
+    double inputValue = [self.inputField.text doubleValue];
+    
+    self.convertButton.enabled = YES;
+    
+    double outputUSD = inputValue * currencies.USD;
+    NSString *tempUSD = [NSString stringWithFormat:@"%.2f",outputUSD];
+    
+    double outputGBP = inputValue * currencies.GBP;
+    NSString *tempGBP = [NSString stringWithFormat:@"%.2f",outputGBP];
+    
+    double outputJPY = inputValue * currencies.JPY;
+    NSString *tempJPY = [NSString stringWithFormat:@"%.2f",outputJPY];
+
+    self.outputLabelUSD.text = tempUSD;
+    self.outputLabelGBP.text = tempGBP;
+    self.outputLabelJPY.text = tempJPY;
+    
 }
 
 @end
